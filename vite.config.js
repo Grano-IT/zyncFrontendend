91addd2bb5a029b668bedfb2378353f3c3442e58
arun-19n <nuraruran32@gmail.com>
2025-03-17 10:08:53 +0530

    Deployed latest frontend update

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/zyncFrontend/',
+  base: "/zyncFrontend/",  // Set to your repo name if using GitHub Pages
 });
